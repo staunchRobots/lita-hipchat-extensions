4eec0f37915bfa7ed9ee7ebfd5d9de396a45dd3b
Leonardo Bighetti <leonardo.bighetti@staunchrobots.com>
2014-07-12 17:47:44 -0300

    added #when command

--- lib/lita/handlers/hipchat-extensions/timezone.rb
+++ lib/lita/handlers/hipchat-extensions/timezone.rb
@@ -3,35 +3,53 @@ module Lita
     class HipchatExtensions < Handler
       # Handles Timezone shenanigans
       class Timezone < Base
-        route /^tz\s+(@\w+)/, :show, command: true
-        route /^tz\s+list/,   :list, command: true
+        route /^tz\s+(@\w+)/,      :show, command: true
+        route /^tz\s+list/,        :list, command: true
+        route /^tz\s+when\s+(.*)/, :when, command: true
 
         # TODO: Document me
         def show(response)
-          log.info "Timezone#show"
           mention_name = response.args.first.gsub "@", ""
           response.reply t("timezones.show.usage") and return unless mention_name
           user = Lita::User.fuzzy_find(mention_name)
           response.reply "#{mention_name}'s timezone is #{format_timezone(user.metadata["timezone"])}" 
         end
 
-        def whenis(response)
-
+        # TODO: Document me
+        def when(response)
+          time = response.args[1]
+          response.reply ("timezones.whenis.usage") and return unless time
+          Time.zone    = current_timezone(response)
+          current_time = time == "now" ? Time.zone.now : Time.zone.parse(time)
+          user_timezones.each do |tz|
+            converted_time = current_time.in_time_zone(ActiveSupport::TimeZone[tz])
+            response.reply "#{tz}: #{format_time(converted_time)}"
+          end
         end
 
+        # TODO: Document me
         def list(response)
-          log.info "Timezone#list"
-          response.reply lita_users.map { |u| u.metadata["timezone"] }.uniq.sort
+          response.reply user_timezones
         end
 
         protected
 
-        def fetch_time(user, time, target)
-          user   = Lita::User.fuzzy_find(user)
-          target = Lita::User.fuzzy_find(target)
+        def user_timezones
+          lita_users.map { |u| u.metadata["timezone"] }.uniq.sort
+        end
+
+        def current_timezone(response)
+          fetch_timezone(response.user.mention_name)
+        end
+
+        def fetch_timezone(mention_name)
+          ActiveSupport::TimeZone[ Lita::User.fuzzy_find(mention_name).metadata["timezone"] ]
+        end
+
+        def fetch_time(source_tz, target_tz)
           # Fetch the timezones
-          tz_user   = ActiveSupport::TimeZone[ fetch_timezone(user)   ]
-          tz_target = ActiveSupport::TimeZone[ fetch_timezone(target) ]
+          tz_source = fetch_timezone(source)
+          tz_target = fetch_timezone(target)
           # Check that we have them
           raise "Nope" unless tz_user && tz_target
           Time.zone = tz_user
